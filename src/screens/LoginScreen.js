9c0fb60ef0b3bdc93bcef2b0ad350b809c61bce1
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-13 22:46:07 +0800

    Revised Login Screen UI

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,92 +1,167 @@
 import React, {Component} from 'react';
 import {
+  View,
   Text,
-  Input,
-  Icon,
-  Button,
-  Card,
-  SocialIcon,
-  Image,
-} from 'react-native-elements';
-import {View, StyleSheet} from 'react-native';
+  TouchableOpacity,
+  TextInput,
+  Platform,
+  StyleSheet,
+  ScrollView,
+  StatusBar,
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
 import {observer, inject} from 'mobx-react';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import {Container} from 'native-base';
-
+import {Icon, Image} from 'react-native-elements';
 @inject('generalStore')
 @observer
 class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+      check_textInputChange: false,
+      secureTextEntry: true,
+    };
+  }
+
+  textInputChange = (val) => {
+    if (val.length !== 0) {
+      this.setState({
+        username: val,
+        check_textInputChange: true,
+      });
+    } else {
+      this.setState({
+        username: val,
+        check_textInputChange: false,
+      });
+    }
+  };
+
+  handlePasswordChange = (val) => {
+    this.setState({
+      password: val,
+    });
+  };
+
+  updateSecureTextEntry = () => {
+    this.setState({
+      secureTextEntry: !this.state.secureTextEntry,
+    });
+  };
+
   render() {
     const {iconPrefix} = this.props.generalStore;
 
     return (
-      <KeyboardAwareScrollView
-        contentContainerStyle={styles.container}
-        style={{backgroundColor: '#E91E63'}}>
-        <Container style={styles.container}>
-          <View style={styles.header}>
-            <Image
-              source={require('../../assets/loginLogo.png')}
-              style={{width: 200, height: 150, resizeMode: 'center'}}
-            />
-            <Text style={{fontSize: 16, color: '#fff'}}>Welcome!</Text>
-          </View>
-          <Card containerStyle={styles.footer}>
-            <View>
-              <View style={styles.formContainer}>
-                <Input
-                  placeholder="Email Address"
-                  inputContainerStyle={styles.inputContainer}
-                  leftIcon={
-                    <Icon
-                      name={`${iconPrefix}-mail`}
-                      type="ionicon"
-                      size={24}
-                      color="#E91E63"
-                      style={{marginLeft: 10}}
-                    />
-                  }
-                />
-                <Input
-                  placeholder="Password"
-                  secureTextEntry={true}
-                  inputContainerStyle={styles.inputContainer}
-                  leftIcon={
-                    <Icon
-                      name={`${iconPrefix}-key`}
-                      type="ionicon"
-                      size={24}
-                      color="#E91E63"
-                      style={{marginLeft: 10}}
-                    />
-                  }
-                />
-              </View>
-              <Button
-                title="Login"
-                buttonStyle={styles.loginButton}
-                containerStyle={styles.buttonContainer}
+      <View style={styles.container}>
+        <StatusBar backgroundColor="#E91E63" />
+
+        <View style={styles.header}>
+          <Image
+            source={require('../../assets/loginLogo.png')}
+            style={{
+              height: 150,
+              width: 200,
+              resizeMode: 'center',
+            }}
+          />
+        </View>
+        <Animatable.View animation="fadeInUpBig" style={styles.footer}>
+          <ScrollView>
+            <Text style={styles.text_header}>Login</Text>
+
+            <Text style={styles.text_footer}>Email Address</Text>
+            <View style={styles.action}>
+              <Icon name="person-outline" color="#E91E63" size={20} />
+              <TextInput
+                placeholder="Your Email Address"
+                style={styles.textInput}
+                autoCapitalize="none"
+                onChangeText={(val) => this.textInputChange(val)}
               />
-              <Text style={styles.centerText}>Or</Text>
-              <SocialIcon
-                title="Sign In With Facebook"
-                button
-                type="facebook"
-              />
+              {this.state.check_textInputChange ? (
+                <Animatable.View animation="bounceIn">
+                  <Icon name="check-circle" color="green" size={20} />
+                </Animatable.View>
+              ) : null}
             </View>
-            <View style={styles.signUpContainer}>
-              <Text style={styles.centerText}>New to Marketeer?</Text>
-              <Button
-                title="Sign Up"
-                type="outline"
-                titleStyle={styles.signUpButtonTitleStyle}
-                buttonStyle={styles.signUpButton}
-                containerStyle={styles.buttonContainer}
+
+            <Text
+              style={[
+                styles.text_footer,
+                {
+                  marginTop: 35,
+                },
+              ]}>
+              Password
+            </Text>
+            <View style={styles.action}>
+              <Icon name="lock-outline" color="#E91E63" size={20} />
+              <TextInput
+                placeholder="Your Password"
+                secureTextEntry={this.state.secureTextEntry ? true : false}
+                style={styles.textInput}
+                autoCapitalize="none"
+                onChangeText={(val) => this.handlePasswordChange(val)}
               />
+              <TouchableOpacity onPress={this.updateSecureTextEntry}>
+                {this.state.secureTextEntry ? (
+                  <Icon
+                    name={`${iconPrefix}-eye`}
+                    type="ionicon"
+                    color="grey"
+                    size={20}
+                  />
+                ) : (
+                  <Icon
+                    name={`${iconPrefix}-eye-off`}
+                    type="ionicon"
+                    color="grey"
+                    size={20}
+                  />
+                )}
+              </TouchableOpacity>
             </View>
-          </Card>
-        </Container>
-      </KeyboardAwareScrollView>
+
+            <View style={styles.button}>
+              <TouchableOpacity
+                style={[
+                  styles.signIn,
+                  {
+                    borderRadius: 24,
+                    borderColor: '#E91E63',
+                    borderWidth: 1,
+                    marginTop: 15,
+                  },
+                ]}>
+                <Text
+                  style={[
+                    styles.textSign,
+                    {
+                      color: '#E91E63',
+                    },
+                  ]}>
+                  Login
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                paddingTop: 10,
+              }}>
+              <Text>Don't have an account? You can sign up </Text>
+              <TouchableOpacity>
+                <Text style={styles.touchable_text}>here.</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </Animatable.View>
+      </View>
     );
   }
 }
@@ -97,51 +172,75 @@ const styles = StyleSheet.create({
     backgroundColor: '#E91E63',
   },
   header: {
-    flex: 1,
-    justifyContent: 'space-evenly',
+    flex: 2,
     alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 10,
   },
   footer: {
-    flex: 2,
-    backgroundColor: '#ffff',
-    flexDirection: 'column',
+    flex: Platform.OS === 'ios' ? 5 : 7,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  text_header: {
+    color: '#111',
+    fontWeight: 'normal',
+    fontSize: 30,
+    paddingBottom: 20,
+  },
+  text_subtext: {
+    color: 'grey',
+    textAlign: 'left',
     justifyContent: 'flex-start',
-    margin: 0,
-    elevation: 20,
-    borderRadius: 30,
-    paddingTop: 50,
-    paddingBottom: 300,
-    marginBottom: -300,
-    paddingHorizontal: 30,
+    alignSelf: 'flex-start',
   },
-  signUpContainer: {bottom: 0},
-  formContainer: {marginBottom: 15},
-  inputContainer: {
-    borderRadius: 24,
-    borderColor: '#E91E63',
-    borderWidth: 1,
+  text_footer: {
+    color: '#E91E63',
+    fontSize: 18,
   },
-  buttonContainer: {
-    alignSelf: 'stretch',
-    marginHorizontal: 10,
-  },
-  loginButton: {
-    backgroundColor: '#E91E63',
-    borderRadius: 24,
-    height: 50,
-  },
-  signUpButton: {
-    borderColor: '#E91E63',
-    borderRadius: 24,
-    borderWidth: 1,
-    height: 50,
-  },
-  signUpButtonTitleStyle: {
+  touchable_text: {
+    fontWeight: 'bold',
     color: '#E91E63',
   },
-  centerText: {
-    textAlign: 'center',
-    marginVertical: 10,
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: '#E91E63',
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  signIn: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  textPrivate: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 20,
+  },
+  color_textPrivate: {
+    color: 'grey',
   },
 });
 
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -2,9 +2,7 @@ import React, {Component} from 'react';
 import {
   View,
   Text,
-  Button,
   TouchableOpacity,
-  Dimensions,
   TextInput,
   Platform,
   StyleSheet,
