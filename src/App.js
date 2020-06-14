c1125a98dc923e9f3ab3f3730e4e5076e761f3c1
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-15 04:32:26 +0800

    Added navigation theme

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import {Root} from 'native-base';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './navigation/MainStack';
+import {colors} from '../assets/colors';
+import {color} from 'react-native-reanimated';
+
+const NavigationTheme = {
+  dark: false,
+  colors: {
+    primary: colors.primary,
+    background: '#fff',
+    card: '#fff',
+    text: colors.text_primary,
+    border: '#fff',
+  },
+};
 
 export default () => (
   <Root>
-    <NavigationContainer>
+    <NavigationContainer theme={NavigationTheme}>
       <MainStack />
     </NavigationContainer>
   </Root>
