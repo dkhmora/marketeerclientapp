f0e2bb6d50133c88f6625eea5d91420b4a4c9318
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-14 21:33:35 +0800

    Added sliding down menu in Dashboard

--- src/components/BaseHeader.js
+++ src/components/BaseHeader.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
-import {Header, Icon, Button} from 'react-native-elements';
+import {StyleSheet, Platform, View, TouchableOpacity} from 'react-native';
+import {Header, Icon, Button, Text} from 'react-native-elements';
 import {colors} from '../../assets/colors';
+import * as Animatable from 'react-native-animatable';
 
 class BaseHeader extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showLocation: false,
+    };
+
+    Animatable.initializeRegistryWithDefinitions({
+      slideIn: {
+        from: {translateY: -200},
+        to: {translateY: 0},
+      },
+      slideOut: {
+        from: {translateY: 0},
+        to: {translateY: -200},
+      },
+    });
   }
 
-  leftIcon = () => {
+  menuIcon = () => {
     const {navigation} = this.props;
 
     if (navigation) {
@@ -27,17 +43,88 @@ class BaseHeader extends Component {
     return null;
   };
 
-  render() {
+  centerComponent = () => {
+    const {centerComponent, title} = this.props;
+    const {showLocation} = this.state;
+
+    if (centerComponent) {
+      return (
+        <TouchableOpacity
+          style={{flex: 1, flexDirection: 'row'}}
+          onPress={() => {
+            if (!showLocation) {
+              this.view.animate('slideIn');
+            } else {
+              this.view.animate('slideOut');
+            }
+
+            this.setState({showLocation: !showLocation});
+          }}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+            }}>
+            <Text style={styles.titleText}>Deliver To: </Text>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+
+    return <Text style={styles.titleText}>{title}</Text>;
+  };
+
+  rightComponent = () => {
+    const {rightComponent} = this.props;
+
+    if (rightComponent) {
+      return <View style={{flex: 1}}></View>;
+    }
+
+    return <View style={{flex: 1}}></View>;
+  };
+
+  slideDownDrawer = () => {
     return (
-      <Header
-        leftComponent={this.leftIcon}
-        statusBarProps={{
-          barStyle: 'light-content',
-          backgroundColor: colors.primary,
-          translucent: true,
-        }}
-        containerStyle={styles.header}
-      />
+      <View>
+        <Animatable.View
+          ref={(view) => (this.view = view)}
+          duration={200}
+          style={{
+            width: '100%',
+            height: 200,
+            backgroundColor: '#fff',
+            zIndex: -99,
+            translateY: -200,
+            position: 'absolute',
+          }}>
+          <Text>Hello World</Text>
+        </Animatable.View>
+      </View>
+    );
+  };
+
+  render() {
+    const {showLocation} = this.state;
+    return (
+      <View>
+        {this.slideDownDrawer()}
+        <Header
+          placement={Platform.OS === 'ios' ? 'center' : 'left'}
+          leftComponent={this.menuIcon}
+          centerComponent={this.centerComponent}
+          rightComponent={this.rightComponent}
+          statusBarProps={{
+            barStyle: 'light-content',
+            backgroundColor: colors.primary,
+            translucent: true,
+          }}
+          containerStyle={styles.header}
+          centerContainerStyle={{
+            flex: 3,
+          }}
+        />
+      </View>
     );
   }
 }
@@ -50,6 +137,10 @@ const styles = StyleSheet.create({
   buttonContainer: {
     borderRadius: 24,
     color: '#fff',
+  },
+  titleText: {
+    fontSize: 16,
+    color: colors.icons,
   },
 });
 
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,15 +1,41 @@
 import React, {Component} from 'react';
-import {View, ScrollView, StyleSheet} from 'react-native';
-import {Header} from 'react-native-elements';
+import {View, ScrollView, StyleSheet, TouchableOpacity} from 'react-native';
+import {Header, Text} from 'react-native-elements';
 import BaseHeader from '../components/BaseHeader';
+import * as Animatable from 'react-native-animatable';
 
 class MainScreen extends Component {
+  deliveryLocationMenu = () => {
+    let showLocation = false;
+
+    return (
+      <View>
+        <TouchableOpacity onPress={() => (showLocation = !showLocation)}>
+          <View style={{flex: 1}}>
+            <Text>Deliver To: </Text>
+          </View>
+        </TouchableOpacity>
+        <Animatable.View animation="slideInDown">
+          {showLocation && (
+            <View style={{width: '100%', height: 300, backgroundColor: '#fff'}}>
+              <Text>Hello</Text>
+            </View>
+          )}
+        </Animatable.View>
+      </View>
+    );
+  };
+
   render() {
     const {navigation} = this.props;
 
     return (
       <View style={styles.container}>
-        <BaseHeader navigation={navigation} />
+        <BaseHeader
+          title="Deliver"
+          centerComponent={this.deliveryLocationMenu}
+          navigation={navigation}
+        />
         <ScrollView></ScrollView>
       </View>
     );
