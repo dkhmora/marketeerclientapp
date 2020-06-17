dc83ceba5f912b44c0e18d67f31cc5a1f9b1b09f
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-17 22:16:41 +0800

    Made Store Cards pressable

--- src/components/StoreCard.js
+++ src/components/StoreCard.js
@@ -2,7 +2,15 @@ import React, {Component} from 'react';
 import {Text, Rating} from 'react-native-elements';
 import storage from '@react-native-firebase/storage';
 import FastImage from 'react-native-fast-image';
-import {View, StyleSheet, Platform} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  TouchableNativeFeedback,
+} from 'react-native';
 import {Card, CardItem} from 'native-base';
 import {colors} from '../../assets/colors';
 import {styles} from '../../assets/styles';
@@ -67,175 +75,179 @@ class StoreCard extends Component {
     const {displayImageUrl, coverImageUrl} = this.state;
 
     return (
-      <Card
-        style={{
-          padding: 0,
-          margin: 0,
-          borderRadius: 8,
-          elevation: 2,
-        }}>
-        {{coverImageUrl} && (
-          <View style={{height: 200}}>
+      <TouchableOpacity
+        activeOpacity={0.85}
+        onPress={() => console.log('yessss')}>
+        <Card
+          style={{
+            padding: 0,
+            margin: 0,
+            borderRadius: 8,
+            elevation: 2,
+          }}>
+          {{coverImageUrl} && (
+            <View style={{height: 200}}>
+              <FastImage
+                source={{uri: coverImageUrl}}
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  height: 200,
+                  borderTopLeftRadius: 8,
+                  borderTopRightRadius: 8,
+                }}
+                resizeMode={FastImage.resizeMode.cover}
+              />
+              <View
+                style={{
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  position: 'absolute',
+                  borderTopLeftRadius: 8,
+                  borderBottomLeftRadius: 8,
+                  borderColor: 'rgba(0,0,0,0.3)',
+                  borderWidth: 1,
+                  right: -1,
+                  padding: 7,
+                  marginTop: 20,
+                  backgroundColor: colors.icons,
+                }}>
+                <Text style={{color: colors.text_primary}}>
+                  {store.deliveryType}
+                </Text>
+                {store.freeDelivery && (
+                  <Text
+                    style={{
+                      color: colors.text_primary,
+                      fontFamily: 'ProductSans-Black',
+                    }}>
+                    Free Delivery
+                  </Text>
+                )}
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  position: 'absolute',
+                  borderTopLeftRadius: 8,
+                  borderBottomRightRadius: 8,
+                  top: 0,
+                  left: 0,
+                  padding: 7,
+                  backgroundColor: colors.primary,
+                }}>
+                <Text style={{color: colors.icons, fontSize: 17}}>
+                  {store.storeCategory}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  position: 'absolute',
+                  borderTopLeftRadius: 8,
+                  borderBottomLeftRadius: 8,
+                  bottom: 60,
+                  right: 0,
+                  padding: 5,
+                  backgroundColor: colors.primary,
+                }}>
+                <Rating
+                  type="custom"
+                  fractions={1}
+                  startingValue={3.3}
+                  imageSize={20}
+                  ratingImage={require('../../assets/images/star.png')}
+                  readonly
+                  tintColor={colors.primary}
+                  ratingColor={colors.accent}
+                  ratingBackgroundColor="#455A64"
+                />
+              </View>
+            </View>
+          )}
+          <CardItem
+            style={{
+              flexDirection: 'column',
+              width: '100%',
+              height: 65,
+              borderRadius: 8,
+              paddingBottom: 20,
+              position: 'relative',
+              bottom: 40,
+            }}>
+            <View
+              style={{
+                alignSelf: 'flex-start',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+              }}>
+              <Text
+                numberOfLines={1}
+                style={[
+                  styles.text_footer,
+                  {
+                    fontFamily: 'ProductSans-Regular',
+                    flex: 1,
+                    textAlign: 'left',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                {store.storeName}
+              </Text>
+            </View>
+
+            <View>
+              <Text
+                numberOfLines={2}
+                style={[
+                  styles.text_subtext,
+                  {
+                    fontFamily: 'ProductSans-light',
+                    textAlign: 'left',
+                    alignSelf: 'flex-start',
+                    flex: 1,
+                  },
+                ]}>
+                {store.storeDescription}
+              </Text>
+            </View>
+
+            <View
+              style={{
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-end',
+                marginTop: 5,
+              }}>
+              <this.PaymentMethods />
+            </View>
+          </CardItem>
+          {{displayImageUrl} && (
             <FastImage
-              source={{uri: coverImageUrl}}
+              source={{uri: displayImageUrl}}
               style={{
                 position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                height: 200,
-                borderTopLeftRadius: 8,
-                borderTopRightRadius: 8,
+                bottom: 95,
+                left: 20,
+                width: 80,
+                height: 80,
+                borderRadius: 8,
+                borderWidth: 1,
+                borderColor: colors.primary,
               }}
               resizeMode={FastImage.resizeMode.cover}
             />
-            <View
-              style={{
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                position: 'absolute',
-                borderTopLeftRadius: 8,
-                borderBottomLeftRadius: 8,
-                borderColor: 'rgba(0,0,0,0.3)',
-                borderWidth: 1,
-                right: -1,
-                padding: 7,
-                marginTop: 20,
-                backgroundColor: colors.icons,
-              }}>
-              <Text style={{color: colors.text_primary}}>
-                {store.deliveryType}
-              </Text>
-              {store.freeDelivery && (
-                <Text
-                  style={{
-                    color: colors.text_primary,
-                    fontFamily: 'ProductSans-Black',
-                  }}>
-                  Free Delivery
-                </Text>
-              )}
-            </View>
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-                position: 'absolute',
-                borderTopLeftRadius: 8,
-                borderBottomRightRadius: 8,
-                top: 0,
-                left: 0,
-                padding: 7,
-                backgroundColor: colors.primary,
-              }}>
-              <Text style={{color: colors.icons, fontSize: 17}}>
-                {store.storeCategory}
-              </Text>
-            </View>
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-                position: 'absolute',
-                borderTopLeftRadius: 8,
-                borderBottomLeftRadius: 8,
-                bottom: 60,
-                right: 0,
-                padding: 5,
-                backgroundColor: colors.primary,
-              }}>
-              <Rating
-                type="custom"
-                fractions={1}
-                startingValue={3.3}
-                imageSize={20}
-                ratingImage={require('../../assets/images/star.png')}
-                readonly
-                tintColor={colors.primary}
-                ratingColor={colors.accent}
-                ratingBackgroundColor="#455A64"
-              />
-            </View>
-          </View>
-        )}
-        <CardItem
-          style={{
-            flexDirection: 'column',
-            width: '100%',
-            height: 65,
-            borderRadius: 8,
-            paddingBottom: 20,
-            position: 'relative',
-            bottom: 40,
-          }}>
-          <View
-            style={{
-              alignSelf: 'flex-start',
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-            }}>
-            <Text
-              numberOfLines={1}
-              style={[
-                styles.text_footer,
-                {
-                  fontFamily: 'ProductSans-Regular',
-                  flex: 1,
-                  textAlign: 'left',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              {store.storeName}
-            </Text>
-          </View>
-
-          <View>
-            <Text
-              numberOfLines={2}
-              style={[
-                styles.text_subtext,
-                {
-                  fontFamily: 'ProductSans-light',
-                  textAlign: 'left',
-                  alignSelf: 'flex-start',
-                  flex: 1,
-                },
-              ]}>
-              {store.storeDescription}
-            </Text>
-          </View>
-
-          <View
-            style={{
-              width: '100%',
-              flexDirection: 'row',
-              justifyContent: 'flex-start',
-              alignItems: 'flex-end',
-              marginTop: 5,
-            }}>
-            <this.PaymentMethods />
-          </View>
-        </CardItem>
-        {{displayImageUrl} && (
-          <FastImage
-            source={{uri: displayImageUrl}}
-            style={{
-              position: 'absolute',
-              bottom: 95,
-              left: 20,
-              width: 80,
-              height: 80,
-              borderRadius: 8,
-              borderWidth: 1,
-              borderColor: colors.primary,
-            }}
-            resizeMode={FastImage.resizeMode.cover}
-          />
-        )}
-      </Card>
+          )}
+        </Card>
+      </TouchableOpacity>
     );
   }
 }
