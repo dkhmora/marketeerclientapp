d8cfed9520f341b1940e2b6687d768239caa9802
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-23 19:56:55 +0800

    Set up cart screen

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -25,31 +25,33 @@ class CartScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar backgroundColor={colors.primary} />
+        <StatusBar translucent backgroundColor="rgba(0, 0, 0, 0.10)" />
 
         <View
           style={{
-            flex: 2,
-            alignItems: 'center',
-            justifyContent: 'flex-end',
+            height: 80,
+            flexDirection: 'row',
+            alignItems: 'flex-end',
+            justifyContent: 'center',
             paddingHorizontal: 20,
             paddingBottom: 10,
           }}>
           <Image
-            source={require('../../assets/images/logo.png')}
+            source={require('../../assets/images/logo_cart.png')}
             style={{
-              height: 150,
-              width: 200,
+              height: 60,
+              width: 80,
               resizeMode: 'center',
+              marginRight: 10,
             }}
           />
+          <Text style={{color: colors.icons, fontSize: 30}}>Cart</Text>
         </View>
         <Animatable.View
           useNativeDriver
           animation="fadeInUpBig"
           style={styles.footer}>
           <ScrollView>
-            <Text style={styles.text_header}>Cart</Text>
             <CartStoreList />
           </ScrollView>
         </Animatable.View>
@@ -57,20 +59,63 @@ class CartScreen extends Component {
           useNativeDriver
           animation="fadeInUpBig"
           style={{
-            flex: 1,
+            flexDirection: 'row',
+            height: 100,
             position: 'absolute',
             bottom: 0,
             left: 0,
             right: 0,
+            alignItems: 'center',
+            justifyContent: 'flex-start',
             backgroundColor: colors.primary,
             borderTopLeftRadius: 30,
             borderTopRightRadius: 30,
+            paddingVertical: 10,
             paddingHorizontal: 20,
-            paddingTop: 10,
           }}>
-          <ScrollView>
-            <Text style={styles.text_header}>Checkout</Text>
-          </ScrollView>
+          <View
+            style={{
+              marginRight: 10,
+              borderRadius: 24,
+              borderWidth: 1,
+              borderColor: colors.icons,
+              padding: 10,
+            }}>
+            <Text
+              style={{
+                fontFamily: 'ProductSans-Black',
+                color: colors.icons,
+                fontSize: 26,
+              }}>
+              P 123123
+            </Text>
+            <Text
+              style={{
+                color: colors.icons,
+                fontSize: 16,
+                alignSelf: 'center',
+              }}>
+              20 Items
+            </Text>
+          </View>
+          <Button
+            raised
+            icon={<Icon name="arrow-right" color={colors.icons} />}
+            iconRight
+            title="Checkout"
+            titleStyle={{
+              color: colors.icons,
+              fontFamily: 'ProductSans-Black',
+              fontSize: 25,
+              marginRight: '20%',
+            }}
+            buttonStyle={{flex: 1}}
+            containerStyle={{
+              flex: 1,
+              borderRadius: 24,
+              padding: 0,
+            }}
+          />
         </Animatable.View>
       </View>
     );
