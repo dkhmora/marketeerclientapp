bc47935e9dfab089f4d16038687497ec59bc2936
darylmora <daryl_kiel_mora@yahoo.com>
2020-07-02 18:27:09 +0800

    Added chevron to storecategorycard and fixed spacing

--- src/components/StoreCategoryCard.js
+++ src/components/StoreCategoryCard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native-elements';
+import {Text, Icon} from 'react-native-elements';
 import {View, TouchableOpacity} from 'react-native';
 import {Card} from 'native-base';
 import FastImage from 'react-native-fast-image';
@@ -41,21 +41,30 @@ class StoreCategoryCard extends Component {
         <TouchableOpacity
           activeOpacity={0.85}
           style={{flex: 1, flexDirection: 'row'}}>
-          <View style={{flex: 3, elevation: 10}}>
+          <View style={{flex: 7, elevation: 10}}>
             <FastImage source={url} style={{flex: 1, borderRadius: 10}} />
           </View>
 
           <View
             style={{
-              flex: 1,
+              flex: 3,
+              flexDirection: 'row',
               alignItems: 'center',
-              justifyContent: 'flex-end',
+              justifyContent: 'space-between',
               padding: 10,
             }}>
             <Text
-              style={{fontSize: 20, color: colors.icons, textAlign: 'center'}}>
+              adjustsFontSizeToFit
+              style={{
+                flex: 1,
+                fontSize: 20,
+                color: colors.icons,
+                textAlign: 'center',
+              }}>
               {item.name}
             </Text>
+
+            <Icon name="chevron-right" color={colors.icons} />
           </View>
         </TouchableOpacity>
       </Card>
